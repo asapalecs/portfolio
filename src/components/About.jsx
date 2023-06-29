eeac85596bae3087799475b8160b166aa3d1a756
Alex <asapalecs@gmail.com>
2023-06-29 23:14:09 +0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi, I'm Alex, nice to meet you. Take a look around.</p>
+          </div>
+          <div>
+            <p>
+              If you had a software expert at your disposal, what could you
+              accomplish? I am dedicated to crafting exceptional software
+              solutions that enhance the lives of those in my vicinity. My
+              expertise lies in developing software for a diverse range of
+              clients, including individuals, small businesses, and large
+              enterprises.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#fb6f92]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Alexandru Cristinescu
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I specialize in developing exceptional digital experiences as a
+          full-stack web developer. Currently, my main focus lies in creating
+          responsive full-stack web applications.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#fb6f92] hover:border-[#fb6f92]">
+            View Work
+            <span className="group-hover:rotate-90 duration-300 flex items-center">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
